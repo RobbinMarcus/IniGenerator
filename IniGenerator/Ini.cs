d6c91cfa92ac5ccbaede2841f47769a9000edb65
Robbin Marcus <robbin.marcus@gmail.com>
2017-04-27 19:51:58 +0200

    IniGenerator source

---
+++ IniGenerator/Ini.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IniGenerator
+{
+    public abstract class Ini
+    {
+        protected readonly IniFile IniFile;
+
+        protected Ini(string filename)
+        {
+            IniFile = new IniFile(filename);
+            LoadIni(filename);
+        }
+
+        protected void LoadIni(string filename)
+        {
+            if (!IniFile.FileExists)
+            {
+                GenerateDefaultIni();
+                IniFile.Save();
+            }
+            else
+            {
+                var allPropertiesSet = true;
+
+                // Check if the property exists in the current ini data
+                foreach (var property in GetType().GetProperties())
+                {
+                    var propertyExists = false;
+
+                    // Check if the property exists in any section
+                    foreach (var section in IniFile.Data.Sections)
+                    {
+                        if (IniFile.Data.Sections[section.SectionName].ContainsKey(property.Name))
+                        {
+                            propertyExists = true;
+                            break;
+                        }
+                    }
+
+                    allPropertiesSet = propertyExists;
+                    if (!allPropertiesSet)
+                        break;
+                }
+
+                // Create a new ini file and merge our current values in it
+                if (!allPropertiesSet)
+                {
+                    GenerateDefaultIni();
+                    var oldIni = new IniFile(filename);
+                    IniFile.Merge(oldIni.Data);
+                    IniFile.Save();
+                }
+            }
+        }
+
+        protected abstract void GenerateDefaultIni();
+    }
+}
---
+++ IniGenerator/IniFile.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using IniParser;
+using IniParser.Model;
+
+namespace IniGenerator
+{
+    public class IniFile
+    {
+        private readonly FileIniDataParser parser;
+        private readonly IniData data;
+        private readonly string path;
+
+        public bool FileExists => File.Exists(path);
+        public IniData Data => data;
+
+        public IniFile(string filePath)
+        {
+            parser = new FileIniDataParser();
+            path = filePath;
+            data = FileExists ? parser.ReadFile(filePath) : new IniData();
+        }
+
+        public void Save()
+        {
+            parser.WriteFile(path, data);
+        }
+
+        public void Merge(IniData newData)
+        {
+            data.Merge(newData);
+        }
+
+        public string GetValue(string key, string name)
+        {
+            return data[key][name];
+        }
+
+        public void SetValue(string key, string name, string value)
+        {
+            data[key][name] = value;
+        }
+    }
+}
